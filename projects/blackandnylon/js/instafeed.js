44640031fd70a7ee9a16322bda376d9ddda6bec9
degio <degio@users.noreply.github.com>
2014-06-09 00:36:33 -0700

    Lost of uPdates

---
+++ projects/blackandnylon/js/instafeed.js
@@ -0,0 +1,54 @@
+var feed;
+
+feed = new Instafeed({
+  get: 'user',
+  userId: 664455077,
+  accessToken: '664455077.22c41e6.3810a3de03424bafb65fe730410c4362',
+  limit: 20,
+          template: '<li><a href="{{link}}" target="_blank" title="{{caption}}"><img src="{{image}}" alt="{{caption}}"/></a><br/><span class="glyphicon glyphicon glyphicon-heart">{{likes}}</span> <span class="glyphicon glyphicon-comment">{{comments}}</span></li>',
+  mock: true,
+  custom: {
+    images: [],
+    currentImage: 0,
+    showImage: function () {
+      var result, image;
+      image = this.options.custom.images[this.options.custom.currentImage];
+      result = this._makeTemplate(this.options.template, {
+        model: image,
+        id: image.id,
+        link: image.link,
+        image: image.images[this.options.resolution].url,
+        caption: this._getObjectProperty(image, 'caption.text'),
+        likes: image.likes.count,
+        comments: image.comments.count,
+        location: this._getObjectProperty(image, 'location.name')
+      });
+      $("#instafeed").html(result);
+    }
+  },
+  success: function (data) {
+    this.options.custom.images = data.data; 
+    this.options.custom.showImage.call(this);
+  }
+});
+feed.run();
+
+$(".next").click(function () {
+  var length, current;
+  current = feed.options.custom.currentImage;
+  length = feed.options.custom.images.length;
+  if (current < length - 1) {
+    feed.options.custom.currentImage++;
+    feed.options.custom.showImage.call(feed);
+  }
+});
+
+$(".prev").click(function () {
+  var length, current;
+  current = feed.options.custom.currentImage;
+  length = feed.options.custom.images.length;
+  if (current > 0) {
+    feed.options.custom.currentImage--
+    feed.options.custom.showImage.call(feed);
+  }
+});
